7de19f3c69c3f5f1e4a46f6d4313678f61412685
Mikko Kumara <mikko.kumara@gmail.com>
2014-01-19 20:11:08 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Moikkelis moi! Tänään muun muassa</p>
+        <ul>
+            <li>Tehdään lista</li>
+            <li>Testaillaan juttuja</li>
+            <li>Syödään sipsejä</li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
